38fcf729e1cdcbd05f99324563f607d392feeb8c
Quenette Christophe <christophe.quenette@etu.umontpellier.fr>
2017-11-08 14:59:57 +0100

    Continue login.php

--- Carpool/login.php
+++ Carpool/login.php
@@ -55,7 +55,7 @@
           <div class="form-group">
             <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
             <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputEmail3" name="email" placeholder="Email">
+              <input type="email" class="form-control" id="inputEmail3" name="email" placeholder="Email" required>
             </div>
           </div>
           <div class="form-group">
