3888561a028efffb30bae96c75cc676e19b43eb8
QUENETTE Christophe <christophe.quenette@gmail.com>
2017-12-05 17:06:16 +0100

    Continue implementation of directions on the google map

--- propose.php
+++ propose.php
@@ -26,22 +26,24 @@
             //     echo "Please log in first to see this page.";
             // }
         ?>
-        <div class="twoParts">
-            <div class="part1">
-                <div class="searchGroup">
-                    <h3>Votre trajet :</h3>
-                    <br>
-                    <form method="post">
-                        <label for="departureCity">De</label>
-                        <input type="text" id="departureCity" class="cities" name="searchDepartureCity" placeholder="Ville de départ">
+        <div class="main">
+            <div class="twoParts">
+                <div class="part1">
+                    <div class="searchGroup">
+                        <h3>Votre trajet :</h3>
                         <br>
-                        <label for="arrivalCity">À</label>
-                        <input type="text" id="arrivalCity" class="cities" name="searchArrivalCity" placeholder="Ville d'arrivée">
-                    </form>
+                        <form method="post">
+                            <label for="departureCity">De</label>
+                            <input type="text" id="departureCity" class="cities" name="searchDepartureCity" placeholder="Ville de départ">
+                            <br>
+                            <label for="arrivalCity">À</label>
+                            <input type="text" id="arrivalCity" class="cities" name="searchArrivalCity" placeholder="Ville d'arrivée">
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <div class="part2">
-                <div id="map"></div>
+                <div class="part2">
+                    <div id="map"></div>
+                </div>
             </div>
         </div>
 
@@ -68,18 +70,16 @@
         function initMap(){
             var directionsService = new google.maps.DirectionsService;
             var directionsDisplay = new google.maps.DirectionsRenderer;
-            var coordMontpellier = {lat: 43.6111, lng: 3.87667};
             var map = new google.maps.Map(document.getElementById('map'), {
-                center: coordMontpellier,
-                zoom: 10
+                center: {lat: 46.227638, lng: 2.213749},
+                zoom: 5
             });
-
             directionsDisplay.setMap(map);
+
             var onChangeHandler = function() {
                 calculateAndDisplayRoute(directionsService, directionsDisplay);
             };
 
-            addMarker(coordMontpellier);
 
             document.getElementById('departureCity').addEventListener('change', onChangeHandler);
             document.getElementById('arrivalCity').addEventListener('change', onChangeHandler);
@@ -99,10 +99,33 @@
             });
         }
 
+        /* PLACE AUTOCCOMPLETE */
 
+        var options = {
+            types: ['(cities)'],
+            componentRestrictions: {country: "fr"}
+        };
+        function activatePlacesSearch1(){
+            var input = document.getElementById('departureCity');
+            var autocomplete = new google.maps.places.Autocomplete(input, options);
+        }
+        function activatePlacesSearch2(){
+            var input = document.getElementById('arrivalCity');
+            var autocomplete = new google.maps.places.Autocomplete(input, options);
+        }
+        function searchPlaces() {
+            activatePlacesSearch1();
+            activatePlacesSearch2();
+        }
+
+        function setAll() {
+            searchPlaces();
+            initMap();
+        }
         </script>
         <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVEJd3oceuJC5ivSIjaWvJomYNxc2JR_A&callback=initMap">
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVEJd3oceuJC5ivSIjaWvJomYNxc2JR_A&libraries=places&callback=setAll">
         </script>
+        <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVEJd3oceuJC5ivSIjaWvJomYNxc2JR_A&libraries=places&callback=searchPlaces"></script> -->
     </body>
 </html>
