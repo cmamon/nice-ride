fb6cd6d8842b2bb89f91cd53ac4eba0127803422
QUENETTE Christophe <christophe.quenette@gmail.com>
2017-12-09 17:04:09 +0100

    Begin building webpage so a member can register a car

---
+++ registerCar.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Enregister une voiture</title>
+        
+    </head>
+    <body>
+
+    </body>
+</html>
