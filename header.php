a5d65e0b0c0b4b4e41cf104c34f7d1c80b3f7a80
QUENETTE Christophe <christophe.quenette@gmail.com>
2017-12-09 17:07:35 +0100

    create a generic file for the web pages (except home page)

---
+++ header.php
@@ -0,0 +1,9 @@
+<div class="defaultNav">
+    <nav>
+        <a id="brand" href="index.php">Nice Ride</a>
+        <ul>
+            <li> <a href="login.php">CONNEXION</a> </li>
+            <li> <a href="signup.php">S'INSCRIRE</a> </li>
+        </ul>
+    </nav>
+</div>
