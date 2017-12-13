66a6ab5b6a2ccc07a5d47df6fa6572e6ac350ba3
Quenette Christophe <christophe.quenette@etu.umontpellier.fr>
2017-12-13 15:17:56 +0100

    Creations of tables

--- SQL/fill.sql
+++ SQL/fill.sql
@@ -1,18 +1,12 @@
-
-ALTER session SET NLS_DATE_FORMAT='DD-MM-YYYY' ;
-
-prompt -------------------------------------------;
-prompt --- Suppression des anciens tuples --------;
-prompt -------------------------------------------;
-
+DELETE FROM USER;
 DELETE FROM CAR;
 DELETE FROM MEMBER;
 DELETE FROM ROUTE;
 DELETE FROM TRIP;
 
 
-INSERT INTO USER VALUES (1, "Christophe", "QUENETTE");
-
-INSERT INTO MEMBER (1, 75, "Avenue",  "Augustin Fliche", "Montpellier", "+33750867489")
-
-INSERT INTO CAR VALUES (1, 1, 'ANDRE', 'FRANCAISE');
+INSERT INTO USER VALUES (1, 'Christophe', 'QUENETTE', 'christophe.quenette@etu.umontpellier.fr');
+INSERT INTO MEMBER VALUES (1, 75, 'Avenue',  'Augustin Fliche', 'Montpellier', '+33750867489', 4.6);
+INSERT INTO CAR VALUES (1, 1, 'Kangoo', 'Renault', 2009, 5);
+INSERT INTO ROUTE VALUES (1, 'Lille', 'Montpellier');
+INSERT INTO TRIP VALUES (1, 1, "2017-12-13");
