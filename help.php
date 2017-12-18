70a2d37e5e25bffdbffb1ffd2f0ebf22daa4bceb
Quenette Christophe <christophe.quenette@etu.umontpellier.fr>
2017-12-18 21:15:07 +0100

    Add an help page

---
+++ help.php
@@ -0,0 +1,24 @@
+<?php require 'functions.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require 'head.php'; ?>
+        <title>Aide (ou pas)</title>
+    </head>
+    <body>
+        <header>
+            <?php require 'header.php'; ?>
+        </header>
+        <div class="main">
+            <div class="emphasized" style="text-align:center">
+                <h3>Cette page ... ne sert à rien.</h3><br>
+                <h3>En effet vous ne trouverez aucune aide ici.</h3><br>
+                <h3>Bonne continuation ^_^.</h3>
+            </div>
+        </div>
+
+        <footer>
+            <?php require 'footer.php'; ?>
+        </footer>
+    </body>
+</html>
