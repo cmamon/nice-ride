e3a99e53fe57b0ac9f20230f4452726f095f0342
Quenette Christophe <christophe.quenette@etu.umontpellier.fr>
2017-12-18 22:36:08 +0100

    Simple search page for trips

--- search.php
+++ search.php
@@ -1,10 +1,37 @@
+<?php require 'functions.php';?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title><Rechercher un trajet/title>
-    </head>
-    <body>
+<head>
+    <?php require 'head.php'; ?>
 
-    </body>
+    <title>Rechercher un trajet</title>
+
+</head>
+<body>
+    <header>
+        <?php require 'header.php'; ?>
+    </header>
+    <div class="main">
+        <div class="searchGroup emphasized">
+            <form action="search.php" method="post">
+                <label for="departureCity">Ville de départ</label><br>
+                <input type="text" id="departureCity" class="cities" name="searchDepartureCity" placeholder="Ville de départ">
+                <br>
+                <label for="departureCity">Ville de d'arrivée</label><br>
+                <input type="text" id="arrivalCity" class="cities" name="searchArrivalCity" placeholder="Ville d'arrivée">
+                <br>
+                <label for="departureCity">Date de votre voyage</label><br>
+                <input type="text" id="datepicker" name="date" placeholder="Date">
+                <br><br>
+                <button type="submit" class="button buttonHome" name="searchButton">Rechercher</button>
+            </form>
+        </div>
+    </div>
+    <footer>
+        <?php require 'footer.php'; ?>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+</body>
 </html>
