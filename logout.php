7b6e77fa1290ad8031e5d154b56b90f508890abd
Quenette Christophe <christophe.quenette@etu.umontpellier.fr>
2017-12-18 21:27:05 +0100

    Page displayed if logout successful

---
+++ logout.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require 'head.php'; ?>
+
+    <title>S'inscrire</title>
+    <meta http-equiv="refresh" content="2;url=index.php" />
+
+</head>
+<body>
+    <header>
+        <?php require 'header.php'; ?>
+    </header>
+
+    <br><br><br>
+        <h3>Vous avez été déconnecté(e) avec succès</h3>
+
+        <footer>
+            <?php require 'footer.php'; ?>
+        </footer>
+        <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
