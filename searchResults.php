6ef9569b06c05a8e8e5c126a4822ca656f7c556a
Quenette Christophe <christophe.quenette@etu.umontpellier.fr>
2017-12-18 21:44:57 +0100

    Determine display of the trips

--- searchResults.php
+++ searchResults.php
@@ -1,4 +1,6 @@
-<?php require 'functions.php'; ?>
+<?php require 'functions.php';
+    $conn = connect_to_db();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,22 +11,23 @@
         <header>
             <?php require 'header.php'; ?>
         </header>
-
-        <div class="sort">
-            <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-                <label for="tri">Trier par</label>
-                <select name="tri">
-                    <option value="triChronologique">Date de Parution</option>
-                    <option value="triPrix">Prix</option>
-                </select>
-
-                <label>
-                    <input type="submit" name="sort" value="Valider">
-                </form>
-        </div>
         <div class="main">
-            <div class="searchResults">
+            <div class="sort">
+                <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+                    <br><br>
+                    <label for="tri">Trier par</label>
+                    <select name="tri">
+                        <option value="triChronologique">Date de Parution</option>
+                        <option value="triPrix">Prix</option>
+                    </select>
 
+                    <label>
+                        <input type="submit" name="sort" value="Valider">
+                </form>
+            </div>
+            <div class="searchResults emphasized">
+                <hr id="tripLimitation">
+                <?php print_trip($conn); ?>
             </div>
         </div>
 
