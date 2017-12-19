809cb07a39f5014ca0c849f0ab28d5dd326e9a68
QUENETTE Christophe <christophe.quenette@gmail.com>
2017-12-19 09:44:03 +0100

    Add require option on input fields

--- headerHomePage.php
+++ headerHomePage.php
@@ -9,9 +9,9 @@
     <h2 id="title">Le covoiturage simple<br><small></small></h2>
     <div class="searchGroup">
         <form action="searchResults.php" method="post">
-            <input type="text" id="departureCity" class="cities" name="searchDepartureCity" placeholder="Ville de départ">
-            <input type="text" id="arrivalCity" class="cities" name="searchArrivalCity" placeholder="Ville d'arrivée">
-            <input type="text" id="datepicker" name="date" placeholder="Date">
+            <input type="text" id="departureCity" class="cities" name="searchDepartureCity" placeholder="Ville de départ" required>
+            <input type="text" id="arrivalCity" class="cities" name="searchArrivalCity" placeholder="Ville d'arrivée" required>
+            <input type="text" id="datepicker" name="date" placeholder="Date" required>
             <button type="submit" class="button buttonHome" name="searchButton">Rechercher</button>
         </form>
     </div>
